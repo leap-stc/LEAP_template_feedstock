0aaae31560c0fc8d718e8184fc7d5e6b1390640f
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-08 13:26:37 -0400

    Add baseline recipe

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # proto_feedstock
 A Prototype feedstock that implements independent metadata and data updates using pangeo forge
+
+## How to run locally
+mamba create -n runner0102 python=3.11 -y
+conda activate runner0102
+pip install pangeo-forge-runner==0.10.2 --no-cache-dir
+
+pangeo-forge-runner bake \
+  --repo=https://github.com/leap-stc/proto_feedstock.git \
+  --ref=main \
+  --feedstock-subdir='feedstock' \
+  --Bake.job_name=proto_a\
+  --Bake.recipe_id=test_short_time_only_chunks\
+  -f config_local.py
---
+++ feedstock/recipe.py
@@ -0,0 +1,42 @@
+"""
+A prototype based on MetaFlux
+"""
+
+import apache_beam as beam
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.transforms import OpenURLWithFSSpec, OpenWithXarray, StoreToZarr, ConsolidateMetadata, ConsolidateCoordinates
+
+# Common Parameters
+dataset_url = 'https://zenodo.org/record/7761881/files'
+
+
+## Monthly version
+input_urls_a = [f'{dataset_url}/METAFLUX_GPP_RECO_monthly_{y}.nc' for y in range(2001, 2003)]
+input_urls_b = [f'{dataset_url}/METAFLUX_GPP_RECO_monthly_{y}.nc' for y in range(2003, 2005)]
+
+pattern_a = pattern_from_file_sequence(input_urls_a, concat_dim='time')
+pattern_b = pattern_from_file_sequence(input_urls_b, concat_dim='time')
+
+proto_a = (
+    beam.Create(pattern_a.items())
+    | OpenURLWithFSSpec()
+    | OpenWithXarray()
+    | StoreToZarr(
+        store_name='proto_a.zarr',
+        combine_dims=pattern_a.combine_dim_keys,
+    )
+    |ConsolidateCoordinates()
+    |ConsolidateMetadata()
+)
+
+proto_b = (
+    beam.Create(pattern_b.items())
+    | OpenURLWithFSSpec()
+    | OpenWithXarray()
+    | StoreToZarr(
+        store_name='proto_b.zarr',
+        combine_dims=pattern_b.combine_dim_keys,
+    )
+    |ConsolidateCoordinates()
+    |ConsolidateMetadata()
+)
