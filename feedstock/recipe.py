6a4db20819ec4c067cab53042e3c941c98accf73
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-08 21:42:19 -0400

    copy stage + get copy target from recipe into script..

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,7 +1,9 @@
 """
 A prototype based on MetaFlux
 """
-
+import zarr
+import os
+from dataclasses import dataclass
 import apache_beam as beam
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import (
@@ -12,9 +14,36 @@ from pangeo_forge_recipes.transforms import (
     ConsolidateDimensionCoordinates,
 )
 
+# copied from cmip feedstock (TODO: move to central repo?)
+@dataclass
+class Copy(beam.PTransform):
+    target_prefix: str
+    
+    def _copy(self,store: zarr.storage.FSStore) -> zarr.storage.FSStore:
+        import os
+        import zarr
+        import gcsfs
+        # We do need the gs:// prefix? 
+        # TODO: Determine this dynamically from zarr.storage.FSStore
+        source = f"gs://{os.path.normpath(store.path)}/" #FIXME more elegant. `.copytree` needs trailing slash
+        target = os.path.join(*[self.target_prefix]+source.split('/')[-3:])
+        # gcs = gcsio.GcsIO()
+        # gcs.copytree(source, target)
+        print(f"HERE: Copying {source} to {target}")
+        fs = gcsfs.GCSFileSystem()
+        fs.cp(source, target, recursive=True)
+        # return a new store with the new path that behaves exactly like the input 
+        # to this stage (so we can slot this stage right before testing/logging stages)
+        return zarr.storage.FSStore(target)
+        
+    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
+        return (pcoll
+            | "Copying Store" >> beam.Map(self._copy)
+        )
+
 # Common Parameters
 dataset_url = 'https://zenodo.org/record/7761881/files'
-
+latest_store_prefix = 'gs://leap-scratch/jbusecke/proto_feedstock/latest/'
 
 ## Monthly version
 input_urls_a = [f'{dataset_url}/METAFLUX_GPP_RECO_monthly_{y}.nc' for y in range(2001, 2003)]
@@ -28,11 +57,15 @@ proto_a = (
     | OpenURLWithFSSpec()
     | OpenWithXarray()
     | StoreToZarr(
-        store_name='proto_a.zarr',
+        store_name='proto-a.zarr', 
+        #FIXME: This is brittle. it needs to be named exactly like in meta.yaml...
+        # Can we inject this in the same way as the root?
+        # Maybe its better to find another way and avoid injections entirely...
         combine_dims=pattern_a.combine_dim_keys,
     )
     |ConsolidateDimensionCoordinates()
     |ConsolidateMetadata()
+    |Copy(target_prefix=latest_store_prefix)
 )
 
 proto_b = (
@@ -40,9 +73,10 @@ proto_b = (
     | OpenURLWithFSSpec()
     | OpenWithXarray()
     | StoreToZarr(
-        store_name='proto_b.zarr',
+        store_name='proto-b.zarr',
         combine_dims=pattern_b.combine_dim_keys,
     )
     |ConsolidateDimensionCoordinates()
     |ConsolidateMetadata()
+    |Copy(target_prefix=latest_store_prefix)
 )
--- scripts/update_attrs.py
+++ scripts/update_attrs.py
@@ -7,6 +7,8 @@ from ruamel.yaml import YAML
 yaml = YAML(typ='safe')
 import gcsfs
 from datetime import datetime, timezone
+from .feedstock.recipe import latest_store_prefix
+print(f"Using {latest_store_prefix=}")
 
 fs = gcsfs.GCSFileSystem()
 
@@ -21,9 +23,14 @@ def update_zarr_attrs(store_path:str, additonal_attrs:dict):
     """
     Update the attributes of a zarr store with the given dictionary
     """
-    store = zarr.open(zarr.storage.FSStore(store_path), mode='a')
-    store.attrs.update(additonal_attrs)
-    zarr.convenience.consolidate_metadata(store_path) #Important: do not pass the store object here!
+        # Check if store exists and otherwise give a useful warning
+    if not fs.exists(store_path):
+        print(f"Warning: Store {store_path} does not exist. Skipping.")
+    else:
+        print(f"Updating {store_path} with {additonal_attrs=}")
+        store = zarr.open(zarr.storage.FSStore(store_path), mode='a')
+        store.attrs.update(additonal_attrs)
+        zarr.convenience.consolidate_metadata(store_path) #Important: do not pass the store object here!
     return store_path
 
 # Loop over each recipe 
@@ -32,16 +39,12 @@ for recipe in meta['recipes']:
     assert 'object' in recipe.keys() #(FIXME: this does not support dict objects yet...)
 
     # Some how get the store path from the recipe
-    store_path = f'gs://leap-scratch/jbusecke/proto_feedstock/{id}.zarr' # how can I extract this for multiple recipes using the config files?
-
-    # Check if store exists and otherwise give a useful warning
-    if not fs.exists(store_path):
-        print(f"Warning: Store {store_path} does not exist. Skipping.")
-        continue
+    store_path = f'{latest_store_prefix}/{id}.zarr'
+    # This only works if the store name in the recipe is the same as the id...which is easy to get wrong!!! 
 
     # add the infor from the top level of the meta.yaml
     top_level_meta = {
-            k: meta.get(k, 'none') for k in [
+            f"pangeo-forge-{k}": meta.get(k, 'none') for k in [
                 'description', 
                 'provenance',
                 'maintainers',
@@ -55,4 +58,3 @@ for recipe in meta['recipes']:
     attr_updates['attrs_updated_time_utc'] = timestamp
     
     check_path = update_zarr_attrs(store_path, attr_updates)
-    print(f"Updated {check_path} with {attr_updates=}")
