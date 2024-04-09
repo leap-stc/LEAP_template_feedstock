c3cf65e9e51d951e02280fe183d10b512a2e5992
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-09 19:13:04 -0400

    Add some checks in the script (bit hacky for now)

--- scripts/create_catalog_zarr.py
+++ scripts/create_catalog_zarr.py
@@ -1,6 +1,7 @@
 # create a 'catalog zarr' for any given recipe
 import os
 import json
+import gcsfs
 import zarr
 import xarray as xr
 from ruamel.yaml import YAML
@@ -16,9 +17,26 @@ recipe_meta = catalog_meta['recipes'][recipe_id]
 data_prefix = catalog_meta['data_store_prefix']
 catalog_prefix = catalog_meta['catalog_store_prefix']
 
-data_store_path = os.path.join(data_prefix,f"{recipe_id}.zarr")
+if 'data_store_path' in recipe_meta.keys() and 'data_store_prefix' in catalog_meta.keys():
+    # raise error if both are present
+    raise ValueError("Both 'data_store_path' and 'data_store_prefix' are present in the recipe metadata. Please only specify one.")
+
+if 'data_store_path' in recipe_meta[recipe_id].keys():
+    data_store_path = recipe_meta[recipe_id]['data_store_path']
+else:
+    data_store_path = os.path.join(data_prefix,f"{recipe_id}.zarr")
+
+# i could specify this fully in the catalog.yaml (that would also accomodate 'external' stores) but for now this is fine
 catalog_store_path = os.path.join(catalog_prefix,f"{recipe_id}-catalog.zarr")
 pyramid_store_path = recipe_meta['pyramid_store_path']
+
+# Check with gcsfs if the data store exists otherwise error out
+fs = gcsfs.GCSFileSystem()
+if not fs.exists(data_store_path):
+    raise FileNotFoundError(f"Data store not found: {data_store_path}")
+
+#TODO: Maybe do the same with the pyramid store? for later, because this is not required.
+
 
 def parse_cf_for_ncviewjs(store: zarr.storage.FSStore) -> zarr.storage.FSStore:
     """Logic to add the necessary cf attributes to a zarr store for ncviewjs"""
@@ -34,5 +52,3 @@ print(f"{catalog_store_path=}")
 
 ds = xr.Dataset(attrs=bake_attrs)
 ds.to_zarr(catalog_store_path, mode='w')
-
-
