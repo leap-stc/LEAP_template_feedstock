3fa86da9eaf8e6142f4a75f5f67888df30131689
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-12 19:50:54 -0400

    Update config_local.py

--- configs/config_local.py
+++ configs/config_local.py
@@ -1,4 +1,4 @@
-c.Bake.prune = 0
+c.Bake.prune = 1
 c.Bake.bakery_class = 'pangeo_forge_runner.bakery.local.LocalDirectBakery'
 BUCKET_PREFIX = "./proto_feedstock/output"
 c.TargetStorage.fsspec_class = "fsspec.implementations.local.LocalFileSystem"
