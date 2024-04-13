712e9faf6dc109efd8664e5448b4e170b29f56c2
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-12 20:49:43 -0400

    Update config_local_hub.py

--- configs/config_local_hub.py
+++ configs/config_local_hub.py
@@ -1,6 +1,6 @@
 c.Bake.prune = 1
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.local.LocalDirectBakery"
-# BUCKET_PREFIX = "gs:leap-scratch/<username>/<feedstock_name>/" # replace <> values and uncomment
+# BUCKET_PREFIX = "gs://leap-scratch/<username>/<feedstock_name>/" # replace <> values and uncomment
 c.TargetStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.InputCacheStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.TargetStorage.root_path = f"{BUCKET_PREFIX}/output/{{job_name}}"
