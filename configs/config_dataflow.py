f267e8167f5da333ea3fa2be0ccd7bb15c993659
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-13 16:19:24 -0400

    Update config_dataflow.py

--- configs/config_dataflow.py
+++ configs/config_dataflow.py
@@ -1,15 +1,14 @@
-FEEDSTOCK_NAME = "proto_feedstock"  # Can we get this at runtime?
-BUCKET_PREFIX = f"gs://leap-scratch/jbusecke/{FEEDSTOCK_NAME}"  # FIXME: Change to data-library eventually
-c.Bake.prune = 0
+# FEEDSTOCK_NAME = "proto_feedstock"  # Can we get this at runtime?
+c.Bake.prune = 1
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.dataflow.DataflowBakery"
+c.DataflowBakery.use_dataflow_prime = False
 c.DataflowBakery.use_public_ips = True
 c.DataflowBakery.service_account_email = (
     "julius-leap-dataflow@leap-pangeo.iam.gserviceaccount.com"
 )
 c.DataflowBakery.project_id = "leap-pangeo"
-c.DataflowBakery.temp_gcs_location = f"{BUCKET_PREFIX}/temp"
-c.DataflowBakery.use_dataflow_prime = False
+c.DataflowBakery.temp_gcs_location = f"gs://leap-scratch/data-library/feedstocks/temp/{FEEDSTOCK_NAME}
 c.TargetStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.InputCacheStorage.fsspec_class = "gcsfs.GCSFileSystem"
-c.TargetStorage.root_path = f"{BUCKET_PREFIX}/{{job_name}}/output"
-c.InputCacheStorage.root_path = f"{BUCKET_PREFIX}/cache/"  # make this a global cache?
+c.TargetStorage.root_path = f"gs://leap-scratch/data-library/feedstocks/output/{FEEDSTOCK_NAME}/{{job_name}}/output"
+c.InputCacheStorage.root_path = f"gs://leap-scratch/data-library/feedstocks/cache"  # make this a global cache?
