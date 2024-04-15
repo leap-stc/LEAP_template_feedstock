406edc2936d53fb636fd96e718a85222f805c3fc
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-15 18:49:58 -0400

    Update config_local_hub.py

--- configs/config_local_hub.py
+++ configs/config_local_hub.py
@@ -1,6 +1,21 @@
+# This logic only works locally on the LEAP-Pangeo hub (or similar Jupyterhubs)
+import os
+user = os.environ['JUPYTERHUB_USER']
+
+#TODO: factor this out into an importable function and import here and in config_local.py
+try:
+    # Run the git command to get the top-level directory path
+    repo_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], text=True).strip()
+    # Use os.path.basename to get the repository name from the path
+    repo_name = os.path.basename(repo_path)
+    return repo_name
+except subprocess.CalledProcessError as e:
+    raise
+  
+BUCKET_PREFIX = f"gs://leap-scratch/{user}/{repo_name}"
+
 c.Bake.prune = 1
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.local.LocalDirectBakery"
-# BUCKET_PREFIX = "gs://leap-scratch/<username>/<feedstock_name>/" # replace <> values and uncomment
 c.TargetStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.InputCacheStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.TargetStorage.root_path = f"{BUCKET_PREFIX}/output/{{job_name}}"
