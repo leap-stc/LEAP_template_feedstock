9c6a28c53012e41e4a867633a89db647e50895ec
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-15 20:21:40 -0400

    Fix config_local_hub.py
    Testing new config setup with https://github.com/jbusecke/metaflux_feedstock and will change things here accordingly

--- configs/config_local_hub.py
+++ configs/config_local_hub.py
@@ -1,5 +1,6 @@
 # This logic only works locally on the LEAP-Pangeo hub (or similar Jupyterhubs)
 import os
+import subprocess
 user = os.environ['JUPYTERHUB_USER']
 
 #TODO: factor this out into an importable function and import here and in config_local.py
@@ -8,11 +9,11 @@ try:
     repo_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], text=True).strip()
     # Use os.path.basename to get the repository name from the path
     repo_name = os.path.basename(repo_path)
-    return repo_name
 except subprocess.CalledProcessError as e:
     raise
 
 BUCKET_PREFIX = f"gs://leap-scratch/{user}/{repo_name}"
+print(f"{BUCKET_PREFIX=}")
 
 c.Bake.prune = 1
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.local.LocalDirectBakery"
