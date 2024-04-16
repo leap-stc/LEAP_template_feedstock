810d2399d268c8e18b5c273d80fb5f84fdaf45aa
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-15 20:08:01 -0400

    Deactivate Copy stage on local run

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -20,7 +20,6 @@ from ruamel.yaml import YAML
 
 yaml = YAML(typ="safe")
 
-
 # copied from cmip feedstock (TODO: move to central repo?)
 @dataclass
 class Copy(beam.PTransform):
@@ -34,11 +33,15 @@ class Copy(beam.PTransform):
         # We do need the gs:// prefix?
         # TODO: Determine this dynamically from zarr.storage.FSStore
         source = f"gs://{os.path.normpath(store.path)}/"  # FIXME more elegant. `.copytree` needs trailing slash
-        fs = gcsfs.GCSFileSystem()  # FIXME: How can we generalize this?
-        fs.cp(source, self.target, recursive=True)
-        # return a new store with the new path that behaves exactly like the input
-        # to this stage (so we can slot this stage right before testing/logging stages)
-        return zarr.storage.FSStore(self.target)
+        if self.target is False:
+            # dont do anything
+            return store
+        else:
+            fs = gcsfs.GCSFileSystem()  # FIXME: How can we generalize this?
+            fs.cp(source, self.target, recursive=True)
+            # return a new store with the new path that behaves exactly like the input
+            # to this stage (so we can slot this stage right before testing/logging stages)
+            return zarr.storage.FSStore(self.target)
 
     def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
         return pcoll | "Copying Store" >> beam.Map(self._copy)
@@ -60,11 +63,25 @@ class InjectAttrs(beam.PTransform):
     ) -> beam.PCollection[zarr.storage.FSStore]:
         return pcoll | "Injecting Attributes" >> beam.Map(self._update_zarr_attrs)
 
+def get_pangeo_forge_build_attrs()-> dict[str, Any]:
+    """Get build information (git hash and time) to add to the recipe output"""
+    # Set up injection attributes
+    # This is for demonstration purposes only and should be discussed with the broader LEAP/PGF community
+    # - Bake in information from the top level of the meta.yaml
+    # - Add a timestamp
+    # - Add the git hash
+    # - Add link to the meta.yaml on main
+    # - Add the recipe id
+
+    git_url_hash = f"{os.environ['GITHUB_SERVER_URL']}/{os.environ['GITHUB_REPOSITORY']}/commit/{os.environ['GITHUB_SHA']}"
+    timestamp = datetime.now(timezone.utc).isoformat()
+    
+    return {
+        "pangeo_forge_build_git_hash": git_url_hash,
+        "pangeo_forge_build_timestamp": timestamp,
+    }
 
 # TODO: Both these stages are generally useful. They should at least be in the utils package, maybe in recipes?
-
-# load the global config values (we will have to decide where these ultimately live)
-catalog_meta = yaml.load(open("feedstock/catalog.yaml"))
 
 
 def find_recipe_meta(catalog_meta: List[Dict[str, str]], r_id: str) -> Dict[str, str]:
@@ -78,22 +95,27 @@ def find_recipe_meta(catalog_meta: List[Dict[str, str]], r_id: str) -> Dict[str,
     )
     return None  # Return None if no matching dictionary is found
 
+# load the global config values (we will have to decide where these ultimately live)
+catalog_meta = yaml.load(open("feedstock/catalog.yaml"))
 
-# Set up injection attributes
-# This is for demonstration purposes only and should be discussed with the broader LEAP/PGF community
-# - Bake in information from the top level of the meta.yaml
-# - Add a timestamp
-# - Add the git hash
-# - Add link to the meta.yaml on main
-# - Add the recipe id
+if os.getenv('GITHUB_ACTIONS') == 'true':
+    print("Running inside GitHub Actions.")
 
-git_url_hash = f"{os.environ['GITHUB_SERVER_URL']}/{os.environ['GITHUB_REPOSITORY']}/commit/{os.environ['GITHUB_SHA']}"
-timestamp = datetime.now(timezone.utc).isoformat()
-
-injection_attrs = {
-    "pangeo_forge_build_git_hash": git_url_hash,
-    "pangeo_forge_build_timestamp": timestamp,
-}
+    # Get final store path from catalog.yaml input
+    target_small = find_recipe_meta(catalog_meta["stores"], "small")["url"]
+    target_large = find_recipe_meta(catalog_meta["stores"], "large")["url"]
+    pgf_build_attrs = get_pangeo_forge_build_attrs()
+else:
+    print("Running locally. Deactivating final copy stage.")
+    # this deactivates the final copy stage for local testing execution
+    target_small = False
+    target_large = False
+    pgf_build_attrs = {}
+    
+print("Final output locations")
+print(f"{target_small=}")
+print(f"{target_large=}")
+print(f"{pgf_build_attrs=}")
 
 ## Monthly version
 input_urls_a = [
@@ -108,11 +130,7 @@ input_urls_b = [
 pattern_a = pattern_from_file_sequence(input_urls_a, concat_dim="time")
 pattern_b = pattern_from_file_sequence(input_urls_b, concat_dim="time")
 
-print(f"{catalog_meta=}")
-target_small = find_recipe_meta(catalog_meta["stores"], "small")["url"]
-target_large = find_recipe_meta(catalog_meta["stores"], "large")["url"]
-print(f"{target_small=}")
-print(f"{target_large=}")
+
 
 # small recipe
 small = (
@@ -126,7 +144,7 @@ small = (
         # Maybe its better to find another way and avoid injections entirely...
         combine_dims=pattern_a.combine_dim_keys,
     )
-    | InjectAttrs(injection_attrs)
+    | InjectAttrs(pgf_build_attrs)
     | ConsolidateDimensionCoordinates()
     | ConsolidateMetadata()
     | Copy(target=target_small)
@@ -141,7 +159,7 @@ large = (
         store_name="large.zarr",
         combine_dims=pattern_b.combine_dim_keys,
     )
-    | InjectAttrs(injection_attrs)
+    | InjectAttrs(pgf_build_attrs)
     | ConsolidateDimensionCoordinates()
     | ConsolidateMetadata()
     | Copy(target=target_large)
