c53e55ce09507197ce9a7a073373684da8345a27
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-17 14:35:34 -0400

    Moving changes to copy stages upstream

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 from typing import List, Dict, Any
 import apache_beam as beam
 from datetime import datetime, timezone
+from leap_data_management_utils.data_management_transforms import Copy, InjectAttrs
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import (
     OpenURLWithFSSpec,
@@ -19,73 +20,6 @@ from pangeo_forge_recipes.transforms import (
 from ruamel.yaml import YAML
 
 yaml = YAML(typ="safe")
-
-
-# copied from cmip feedstock (TODO: move to central repo?)
-@dataclass
-class Copy(beam.PTransform):
-    target: str
-
-    def _copy(self, store: zarr.storage.FSStore) -> zarr.storage.FSStore:
-        import os
-        import zarr
-        import gcsfs
-
-        # We do need the gs:// prefix?
-        # TODO: Determine this dynamically from zarr.storage.FSStore
-        source = f"gs://{os.path.normpath(store.path)}/"  # FIXME more elegant. `.copytree` needs trailing slash
-        if self.target is False:
-            # dont do anything
-            return store
-        else:
-            fs = gcsfs.GCSFileSystem()  # FIXME: How can we generalize this?
-            fs.cp(source, self.target, recursive=True)
-            # return a new store with the new path that behaves exactly like the input
-            # to this stage (so we can slot this stage right before testing/logging stages)
-            return zarr.storage.FSStore(self.target)
-
-    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
-        return pcoll | "Copying Store" >> beam.Map(self._copy)
-
-
-@dataclass
-class InjectAttrs(beam.PTransform):
-    inject_attrs: dict
-
-    def _update_zarr_attrs(self, store: zarr.storage.FSStore) -> zarr.storage.FSStore:
-        # TODO: Can we get a warning here if the store does not exist?
-        attrs = zarr.open(store, mode="a").attrs
-        attrs.update(self.inject_attrs)
-        # ? Should we consolidate here? We are explicitly doing that later...
-        return store
-
-    def expand(
-        self, pcoll: beam.PCollection[zarr.storage.FSStore]
-    ) -> beam.PCollection[zarr.storage.FSStore]:
-        return pcoll | "Injecting Attributes" >> beam.Map(self._update_zarr_attrs)
-
-
-def get_pangeo_forge_build_attrs() -> dict[str, Any]:
-    """Get build information (git hash and time) to add to the recipe output"""
-    # Set up injection attributes
-    # This is for demonstration purposes only and should be discussed with the broader LEAP/PGF community
-    # - Bake in information from the top level of the meta.yaml
-    # - Add a timestamp
-    # - Add the git hash
-    # - Add link to the meta.yaml on main
-    # - Add the recipe id
-
-    git_url_hash = f"{os.environ['GITHUB_SERVER_URL']}/{os.environ['GITHUB_REPOSITORY']}/commit/{os.environ['GITHUB_SHA']}"
-    timestamp = datetime.now(timezone.utc).isoformat()
-
-    return {
-        "pangeo_forge_build_git_hash": git_url_hash,
-        "pangeo_forge_build_timestamp": timestamp,
-    }
-
-
-# TODO: Both these stages are generally useful. They should at least be in the utils package, maybe in recipes?
-
 
 def find_recipe_meta(catalog_meta: List[Dict[str, str]], r_id: str) -> Dict[str, str]:
     # Iterate over each dictionary in the list
