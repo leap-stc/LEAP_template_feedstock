ca7337d1d8b6dc061bbc4642e9952b47acdb1fb1
Julius Busecke <julius@ldeo.columbia.edu>
2024-04-22 15:12:07 -0400

    Testing upstream refactor of provenance injection and catalog url parsing

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -5,8 +5,7 @@ A synthetic prototype recipe
 import os
 from typing import List, Dict, Any
 import apache_beam as beam
-from datetime import datetime, timezone
-from leap_data_management_utils.data_management_transforms import Copy, InjectAttrs
+from leap_data_management_utils.data_management_transforms import Copy, InjectAttrs, check_github_workflow
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import (
     OpenURLWithFSSpec,
@@ -15,63 +14,18 @@ from pangeo_forge_recipes.transforms import (
     ConsolidateMetadata,
     ConsolidateDimensionCoordinates,
 )
-from ruamel.yaml import YAML
+# parse the catalog store locations (this is where the data is copied to after successful write (and maybe testing)
+catalog_store_urls = get_catalog_store_urls("feedstock/catalog.yaml")
 
-yaml = YAML(typ="safe")
-
-# load the global config values (we will have to decide where these ultimately live)
-catalog_meta = yaml.load(open("feedstock/catalog.yaml"))
-
-
-def find_recipe_meta(catalog_meta: List[Dict[str, str]], r_id: str) -> Dict[str, str]:
-    # Iterate over each dictionary in the list
-    for d in catalog_meta:
-        # Check if the 'id' key matches the search_id
-        if d["id"] == r_id:
-            return d
-    print(
-        f"Could not find {r_id=}. Got the following recipe_ids: {[d['id'] for d in catalog_meta]}"
-    )
-    return None  # Return None if no matching dictionary is found
-
-
-def get_pangeo_forge_build_attrs() -> dict[str, Any]:
-    """Get build information (git hash and time) to add to the recipe output"""
-    # Set up injection attributes
-    # This is for demonstration purposes only and should be discussed with the broader LEAP/PGF community
-    # - Bake in information from the top level of the meta.yaml
-    # - Add a timestamp
-    # - Add the git hash
-    # - Add link to the meta.yaml on main
-    # - Add the recipe id
-
-    git_url_hash = f"{os.environ['GITHUB_SERVER_URL']}/{os.environ['GITHUB_REPOSITORY']}/commit/{os.environ['GITHUB_SHA']}"
-    timestamp = datetime.now(timezone.utc).isoformat()
-
-    return {
-        "pangeo_forge_build_git_hash": git_url_hash,
-        "pangeo_forge_build_timestamp": timestamp,
-    }
-
-
+# if not run in a github workflow, assume local testing and deactivate the copy stage by setting all urls to False (see https://github.com/leap-stc/leap-data-management-utils/blob/b5762a17cbfc9b5036e1cd78d62c4e6a50c9691a/leap_data_management_utils/data_management_transforms.py#L121-L145)
 if os.getenv("GITHUB_ACTIONS") == "true":
     print("Running inside GitHub Actions.")
-
-    # Get final store path from catalog.yaml input
-    target_small = find_recipe_meta(catalog_meta["stores"], "small")["url"]
-    target_large = find_recipe_meta(catalog_meta["stores"], "large")["url"]
-    pgf_build_attrs = get_pangeo_forge_build_attrs()
 else:
     print("Running locally. Deactivating final copy stage.")
-    # this deactivates the final copy stage for local testing execution
-    target_small = False
-    target_large = False
-    pgf_build_attrs = {}
+    catalog_store_urls = {k: False for k in catalog_store_urls.keys()}
 
 print("Final output locations")
-print(f"{target_small=}")
-print(f"{target_large=}")
-print(f"{pgf_build_attrs=}")
+print(f"{catalog_store_urls=}")
 
 ## Monthly version
 input_urls_a = [
@@ -99,7 +53,7 @@ small = (
         # Maybe its better to find another way and avoid injections entirely...
         combine_dims=pattern_a.combine_dim_keys,
     )
-    | InjectAttrs(pgf_build_attrs)
+    | InjectAttrs()
     | ConsolidateDimensionCoordinates()
     | ConsolidateMetadata()
     | Copy(target=target_small)
@@ -114,7 +68,7 @@ large = (
         store_name="large.zarr",
         combine_dims=pattern_b.combine_dim_keys,
     )
-    | InjectAttrs(pgf_build_attrs)
+    | InjectAttrs()
     | ConsolidateDimensionCoordinates()
     | ConsolidateMetadata()
     | Copy(target=target_large)
