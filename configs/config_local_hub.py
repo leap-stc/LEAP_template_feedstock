ddb6984dd63d7ca4cbffc86600a3a0738d7fa653
Raphael Hagen <norlandrhagen@gmail.com>
2025-02-11 13:06:51 -0700

    updates config to write to LEAP osn

--- configs/config_local_hub.py
+++ configs/config_local_hub.py
@@ -2,6 +2,8 @@
 import os
 import subprocess
 user = os.environ['JUPYTERHUB_USER']
+access_key_id = os.environ['access_key_id']
+secret_access_key = os.environ['secret_access_key']
 
 #TODO: factor this out into an importable function and import here and in config_local.py
 try:
@@ -17,7 +19,15 @@ print(f"{BUCKET_PREFIX=}")
 
 c.Bake.prune = 1
 c.Bake.bakery_class = "pangeo_forge_runner.bakery.local.LocalDirectBakery"
-c.TargetStorage.fsspec_class = "gcsfs.GCSFileSystem"
 c.InputCacheStorage.fsspec_class = "gcsfs.GCSFileSystem"
-c.TargetStorage.root_path = f"{BUCKET_PREFIX}/output/{{job_name}}"
 c.InputCacheStorage.root_path = f"{BUCKET_PREFIX}/cache/"
+
+s3_args = {
+       "key": access_key_id,
+       "secret": secret_access_key,
+       "client_kwargs":{"endpoint_url":"https://nyu1.osn.mghpcc.org"}
+   }
+
+c.TargetStorage.fsspec_class = "s3fs.S3FileSystem"
+c.TargetStore.fsspec_args = s3_args
+c.TargetStorage.root_path = f"leap-pangeo-pipeline/{repo_name}/"
