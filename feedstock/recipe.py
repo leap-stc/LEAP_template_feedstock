5a29eb6c2297120ab280a0c9b89c336e754577e6
Raphael Hagen <norlandrhagen@gmail.com>
2025-02-11 13:14:08 -0700

    slightly reduce template boilerplate & remove copy stage since we can now write to OSN

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -2,12 +2,8 @@
 A synthetic prototype recipe
 """
 
-import os
-
 import apache_beam as beam
 from leap_data_management_utils.data_management_transforms import (
-    Copy,
-    InjectAttrs,
     get_catalog_store_urls,
 )
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
@@ -22,16 +18,6 @@ from pangeo_forge_recipes.transforms import (
 # parse the catalog store locations (this is where the data is copied to after successful write (and maybe testing)
 catalog_store_urls = get_catalog_store_urls('feedstock/catalog.yaml')
 
-# if not run in a github workflow, assume local testing and deactivate the copy stage by setting all urls to False (see https://github.com/leap-stc/leap-data-management-utils/blob/b5762a17cbfc9b5036e1cd78d62c4e6a50c9691a/leap_data_management_utils/data_management_transforms.py#L121-L145)
-if os.getenv('GITHUB_ACTIONS') == 'true':
-    print('Running inside GitHub Actions.')
-else:
-    print('Running locally. Deactivating final copy stage.')
-    catalog_store_urls = {k: False for k in catalog_store_urls.keys()}
-
-print('Final output locations')
-print(f'{catalog_store_urls=}')
-
 ###########################
 ## Start Modifying here ###
 ###########################
@@ -41,52 +27,24 @@ input_urls_a = [
     'gs://cmip6/pgf-debugging/hanging_bug/file_a.nc',
     'gs://cmip6/pgf-debugging/hanging_bug/file_b.nc',
 ]
-input_urls_b = [
-    'gs://cmip6/pgf-debugging/hanging_bug/file_a_huge.nc',
-    'gs://cmip6/pgf-debugging/hanging_bug/file_b_huge.nc',
-]
-
-pattern_a = pattern_from_file_sequence(input_urls_a, concat_dim='time')
-pattern_b = pattern_from_file_sequence(input_urls_b, concat_dim='time')
 
 
-# small recipe
+file_pattern = pattern_from_file_sequence(input_urls_a, concat_dim='time')
+
+
 small = (
-    beam.Create(pattern_a.items())
+    beam.Create(file_pattern.items())
     | OpenURLWithFSSpec()
     | OpenWithXarray()
     | StoreToZarr(
-        store_name='small.zarr',
-        # FIXME: This is brittle. it needs to be named exactly like in meta.yaml...
-        # Can we inject this in the same way as the root?
-        # Maybe its better to find another way and avoid injections entirely...
-        combine_dims=pattern_a.combine_dim_keys,
-        target_chunks={
-            'time': 1,
-        },
+        # Make sure to change this name!
+        store_name='<name_of_your_dataset>.zarr',
+        # Note:  This name must exactly match the name in meta.yaml
+        combine_dims=file_pattern.combine_dim_keys,
+        # Note: You can modify the chunking structure here. Ex: {'time':-1, 'lat':180, 'lon':360}
+        # You should aim for 100MB chunks
+        target_chunks={},
     )
-    | InjectAttrs()
     | ConsolidateDimensionCoordinates()
     | ConsolidateMetadata()
-    | Copy(target=catalog_store_urls['small'])  # DO NOT REMOVE THIS STAGE. This stage will copy the
-    # resulting zarr array to the location specified in the
-    # `catalog.yaml` file.
-)
-
-# larger recipe
-large = (
-    beam.Create(pattern_b.items())
-    | OpenURLWithFSSpec()
-    | OpenWithXarray()
-    | StoreToZarr(
-        store_name='large.zarr',
-        combine_dims=pattern_b.combine_dim_keys,
-        target_chunks={
-            'time': 1,
-        },
-    )
-    | InjectAttrs()
-    | ConsolidateDimensionCoordinates()
-    | ConsolidateMetadata()
-    | Copy(target=catalog_store_urls['large'])
 )
