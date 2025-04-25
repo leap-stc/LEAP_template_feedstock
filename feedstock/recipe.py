de25359b0d40819b873f10a2b204f3e9d0f4c2b4
norlandrhagen <norlandrhagen@gmail.com>
2025-04-25 13:21:54 -0600

    updates recipes

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,50 +1,33 @@
-"""
-A synthetic prototype recipe
-"""
+"""Example recipe"""
 
-import apache_beam as beam
-from leap_data_management_utils.data_management_transforms import (
-    get_catalog_store_urls,
+import xarray as xr
+from distributed import Client
+
+client = Client()
+client
+
+input_urls = ['https://<data-provider>/../../<file1.nc>']
+
+
+# Note: there are lots of ways to create a Zarr store from a collection of archival files.
+# Chat with the data and compute team if you have questions.
+# The example here opens up a collection of file urls with Xarray's open_mfdataset
+# which.. can be difficult for very large datasets.
+
+ds = xr.open_mfdataset(
+    input_urls, chunks={}, parallel=True, coords='minimal', data_vars='minimal', compat='override'
 )
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
-from pangeo_forge_recipes.transforms import (
-    ConsolidateDimensionCoordinates,
-    ConsolidateMetadata,
-    OpenURLWithFSSpec,
-    OpenWithXarray,
-    StoreToZarr,
-)
 
-# parse the catalog store locations (this is where the data is copied to after successful write (and maybe testing)
-catalog_store_urls = get_catalog_store_urls('feedstock/catalog.yaml')
+# Note: Chunking your dataset is important for your analysis use case! ie. time-series vs spatial analysis
+# A good rule of thumb 100MB chunk sizes.
 
-###########################
-## Start Modifying here ###
-###########################
+ds = ds.chunk({'time': 10, 'lat': 180, 'lon': 360})
 
-## Monthly version
-input_urls_a = [
-    'gs://cmip6/pgf-debugging/hanging_bug/file_a.nc',
-    'gs://cmip6/pgf-debugging/hanging_bug/file_b.nc',
-]
-
-
-file_pattern = pattern_from_file_sequence(input_urls_a, concat_dim='time')
-
-
-small = (
-    beam.Create(file_pattern.items())
-    | OpenURLWithFSSpec()
-    | OpenWithXarray()
-    | StoreToZarr(
-        # Make sure to change this name!
-        store_name='<name_of_your_dataset>.zarr',
-        # Note:  This name must exactly match the name in meta.yaml
-        combine_dims=file_pattern.combine_dim_keys,
-        # Note: You can modify the chunking structure here. Ex: {'time':-1, 'lat':180, 'lon':360}
-        # You should aim for 100MB chunks
-        target_chunks={},
-    )
-    | ConsolidateDimensionCoordinates()
-    | ConsolidateMetadata()
+# Good practice is trying to build a smaller Zarr from a subset of your inputs files for testing.
+# Note: This writes to leap-scratch. Once we all feel good about the recipe, we can write to leap-persistant.
+ds.to_zarr(
+    'gs://leap-scratch/<your_user_name>/<dataset_name.zarr>',
+    zarr_format=3,
+    consolidated=False,
+    mode='w',
 )
