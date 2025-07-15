128ac6f18a72fa9936a3c4ec1a3f7b53d4844c6a
norlandrhagen <norlandrhagen@gmail.com>
2025-07-15 15:01:52 -0600

    adds obstore based OSN writing

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -2,31 +2,51 @@
 
 import xarray as xr
 from distributed import Client
+from obstore.store import S3Store
+from zarr.storage import ObjectStore
 
+# Optional: This will start a dask distributed client for parallel processing.
 client = Client()
-client
+print(client.scheduler.address)
 
+
+# ---------- Input files ------------------
+# Starting with a subset of your input files can be helpful for testing.
 input_urls = ['https://<data-provider>/../../<file1.nc>']
 
 
+# ---------- Combine ----------------------
 # Note: there are lots of ways to create a Zarr store from a collection of archival files.
 # Chat with the data and compute team if you have questions.
 # The example here opens up a collection of file urls with Xarray's open_mfdataset
-# which.. can be difficult for very large datasets.
-
 ds = xr.open_mfdataset(
     input_urls, chunks={}, parallel=True, coords='minimal', data_vars='minimal', compat='override'
 )
 
+# ---------- Chunk ----------------------
 # Note: Chunking your dataset is important for your analysis use case! ie. time-series vs spatial analysis
 # A good rule of thumb 100MB chunk sizes.
-
 ds = ds.chunk({'time': 10, 'lat': 180, 'lon': 360})
 
-# Good practice is trying to build a smaller Zarr from a subset of your inputs files for testing.
-# Note: This writes to leap-scratch. Once we all feel good about the recipe, we can write to leap-persistant.
+
+# ---------- WRITE ----------------------
+# This example writes to LEAP's OSN inbox bucket.
+
+# UPDATE THIS!
+DATASET_NAME = '<INSERT_YOUR_DATASET_NAME_HERE>'
+
+osnstore = S3Store(
+    'leap-pangeo-inbox',
+    prefix=f'{DATASET_NAME}/{DATASET_NAME}.zarr',
+    aws_endpoint='https://nyu1.osn.mghpcc.org',
+    access_key_id='<ASK LEAP DCT MEMBERS FOR CREDENTIALS>',
+    secret_access_key='<ASK LEAP DCT MEMBERS FOR CREDENTIALS>',
+    client_options={'allow_http': True},
+)
+zstore = ObjectStore(osnstore)
+
 ds.to_zarr(
-    'gs://leap-scratch/<your_user_name>/<dataset_name.zarr>',
+    zstore,
     zarr_format=3,
     consolidated=False,
     mode='w',
